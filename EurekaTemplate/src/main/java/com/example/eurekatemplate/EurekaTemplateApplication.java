6c45a781421da32c8f9bc6fb846214f4e9b0d617
최현도 <139307597+chd000125@users.noreply.github.com>
2025-05-14 18:27:17 +0900

    Add files via upload

---
+++ EurekaTemplate/src/main/java/com/example/eurekatemplate/EurekaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaTemplateApplication.class, args);
+    }
+
+}
---
+++ EurekaTemplate/src/test/java/com/example/eurekatemplate/EurekaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
